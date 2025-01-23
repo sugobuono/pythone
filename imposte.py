edf1754f35db5eb937104d9c5136e3c1891d965b
sugobuono <riccardo3tamanti3@gmail.com>
2025-01-23 13:23:10 +0100

    Update imposte.py

--- imposte.py
+++ imposte.py
@@ -2,12 +2,16 @@ PRIMA_ALIQUOTA=0.23
 SECONDA_ALIQUOTA=0.35
 TERZA_ALIQUOTA=0.43
 
-reddito=int(input("inserire il proprio reddito: "))
-if 0<=reddito<=28000:
+base1=28000*0.23
+base2=22000*0.35
+reddito=int(input("inserire il proprio reddito:"))
+if reddito<=28000:
     aliquota=reddito*PRIMA_ALIQUOTA
-elif 28001<=reddito<=50000:
-    aliquota=reddito*SECONDA_ALIQUOTA
+    print("il reddito al netto delle tasse è:",reddito-aliquota)
 else:
-    aliquota=reddito*TERZA_ALIQUOTA
-print(aliquota)
-print("il reddito al netto delle tasse è", reddito-aliquota)
+    if reddito<=50000:
+        aliquota=base1+(reddito-28000)*0.35
+        print("il reddito al netto delle tasse è:",reddito-aliquota)
+    else:
+        aliquota=base1+base2+(reddito-50000)*0.43
+        print("il reddito al netto delle tasse è:",reddito-aliquota)
